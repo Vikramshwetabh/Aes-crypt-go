dbdf4b33eac1f5b124419b352c5cba740c6d17f7
Shwetabh vikram <vikramshwetabh29@gmail.com>
2025-07-21 16:44:31 +0530

    Update main.go

--- prog1/main.go
+++ prog1/main.go
@@ -6,14 +6,13 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
-	"io"
 )
 
 func main() {
 	fmt.Println("AES Encryption Example")
-	text := []byte("very secret text")
+	text := []byte("very secret text") //plain text is converted into byte slice
 	//generating a random key of 16 bytes
-	key := make([]byte, 16)
+	key := make([]byte, 24)
 	_, err := rand.Read(key)
 	if err != nil {
 		panic(err)
@@ -23,19 +22,27 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	//wrapping in a gcm
+	//wrapping in a gcm. use the gcm mode from cipher
 	gcm, err := cipher.NewGCM(c)
 	if err != nil {
 		panic(err)
 	}
 	//create a nonce(IV) used for encryption by gcm
 	nonce := make([]byte, gcm.NonceSize())
-	_, err = io.ReadFull(rand.Reader, nonce)
-	if err != nil {
-		panic(err)
-	}
+	// _, err = io.ReadFull(rand.Reader, nonce)
+	// if err != nil {
+	// 	panic(err)
+	// }s
 
 	//encrpt the text
 	ciphertext := gcm.Seal(nil, nonce, text, nil)
 	fmt.Println("encrypted text", hex.EncodeToString(ciphertext))
+
+	normalText, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("decrypted text:", string(normalText))
+
 }
