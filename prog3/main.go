d430996a7036f45bc0e43f9bfc02507d77cff2fd
Shwetabh vikram <vikramshwetabh29@gmail.com>
2025-07-22 19:48:09 +0530

    updated main

---
+++ prog3/main.go
@@ -0,0 +1,9 @@
+package main
+
+type person struct {
+	Name          string `json:"name"`
+	Mobile        string `json:"mobile"`
+	PAN           string `json:"pan"`
+	Email         string `json:"email"`
+	AadhaarNumber string `json:"aadhaar_number"`
+}
